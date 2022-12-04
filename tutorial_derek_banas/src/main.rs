52767f5830190b77089f3c00c9c9ceccf82fc94b
Armin <halilovic.armin@hotmail.com>
2022-12-04 19:46:05 +0100

    Finished Derek Banas tutorial

--- tutorial_derek_banas/src/main.rs
+++ tutorial_derek_banas/src/main.rs
@@ -3,10 +3,20 @@
 // https://www.youtube.com/watch?v=ygL_xcavzQ4
 
 use rand::Rng;
+use std::borrow::Borrow;
+use std::cell::RefCell;
 use std::cmp::Ordering;
+use std::collections::HashMap;
 use std::fs::File;
 use std::io::{self, BufRead, BufReader, ErrorKind, Write};
-use std::ops::Add; // allows us to perform addition with generics
+use std::ops::Add;
+use std::rc::Rc;
+use std::sync::{Arc, Mutex};
+use std::thread;
+use std::time::Duration;
+
+mod restaurant;
+use crate::restaurant::order_food;
 
 fn greet_user() {
     println!("Name?");
@@ -248,6 +258,270 @@ fn generic_sum<T: Add<Output = T>>(x: T, y: T) -> T {
 // 2. There is only 1 owner at a time.
 // 3. When the owner goes out of scope, the value is deleted from memory.
 
+fn hashmap() {
+    let mut heroes = HashMap::new();
+    heroes.insert("Superman", "Clark Kent");
+    heroes.insert("Batman", "Bruce Wayne");
+    heroes.insert("The Flash", "Barry Allen");
+    println!("{}", heroes.len());
+
+    for (k, v) in heroes.iter() {
+        println!("{}: {}", k, v);
+    }
+
+    if heroes.contains_key("Batman") {
+        match heroes.get("Batman") {
+            Some(x) => println!("Batman is a hero"),
+            None => println!("Batman is not a hero"),
+        }
+    }
+
+    match heroes.get("NotBatman") {
+        Some(x) => println!("NotBatman is a hero"),
+        None => println!("NotBatman is not a hero"),
+    }
+}
+
+fn structs() {
+    struct Customer {
+        name: String,
+        address: String,
+        balance: u32,
+    }
+    let mut bob = Customer {
+        name: String::from("Bob Smith"),
+        address: String::from("555 Main Street"),
+        balance: 23450,
+    };
+    println!("{}", bob.address);
+    bob.address = String::from("505 Main St");
+    println!("{}", bob.address);
+}
+
+fn generic_struct() {
+    struct GenericRectangle<T, U> {
+        length: T,
+        height: U,
+    }
+    let rec = GenericRectangle {
+        length: 4,
+        height: 10.5,
+    };
+    println!("{} {}", rec.length, rec.height);
+
+    let rec = GenericRectangle {
+        length: "abc",
+        height: 'd',
+    };
+    println!("{} {}", rec.length, rec.height);
+}
+
+fn traits() {
+    trait Shape {
+        fn new(length: f32, width: f32) -> Self;
+        fn area(&self) -> f32;
+    }
+
+    struct Rectangle {
+        length: f32,
+        width: f32,
+    }
+
+    struct Circle {
+        length: f32,
+        width: f32,
+    }
+
+    impl Shape for Rectangle {
+        fn new(length: f32, width: f32) -> Rectangle {
+            return Rectangle { length, width };
+        }
+        fn area(&self) -> f32 {
+            return self.length * self.width;
+        }
+    }
+
+    impl Shape for Circle {
+        fn new(length: f32, width: f32) -> Circle {
+            return Circle { length, width };
+        }
+        fn area(&self) -> f32 {
+            return (self.length / 2.0).powf(2.0) * PI;
+        }
+    }
+
+    let rec: Rectangle = Shape::new(10.0, 1.0);
+    let circle: Circle = Shape::new(1.0, 10.0);
+    println!("{}", rec.area());
+    println!("{}", circle.area());
+}
+
+fn files() {
+    let path: &str = "lines.txt";
+    let output: Result<File, io::Error> = File::create(path);
+    let mut output: File = match output {
+        Ok(file) => file,
+        Err(error) => panic!("Problem creating file: {:?}", error),
+    };
+
+    write!(output, "Just some \n random words").expect("Failed to write to file");
+
+    let input: File = File::open(path).unwrap();
+    let buffered: BufReader<File> = BufReader::new(input);
+    buffered
+        .lines()
+        .for_each(|line: Result<String, io::Error>| println!("{}", line.unwrap()));
+
+    let output2: Result<File, io::Error> = File::create("rand.txt");
+    let output2: File = match output2 {
+        Ok(file) => file,
+        Err(error) => match error.kind() {
+            ErrorKind::NotFound => match File::create("rand.txt") {
+                Ok(fc) => fc,
+                Err(e) => panic!("Cannot create file: {:?}", error),
+            },
+            _other => panic!("Problem opening file: {:?}", error),
+        },
+    };
+}
+
+fn closure() {
+    let can_vote = |age: i32| age >= 18;
+    println!("Can vote: {}", can_vote(1));
+    println!("Can vote: {}", can_vote(18));
+
+    let mut i = 5;
+    let print_var = |i: &i32| println!("i: {}", i);
+    print_var(&i);
+    i = 10;
+    print_var(&i);
+    let mut change_var = || i += 1;
+    change_var();
+    print_var(&i);
+
+    let sum = |a, b| a + b;
+    let prod = |a, b| a * b;
+    println!("{}", use_function(3, 8, sum));
+    println!("{}", use_function(3, 8, prod));
+}
+
+fn use_function<T>(a: i32, b: i32, function: T) -> i32
+where
+    T: Fn(i32, i32) -> i32,
+{
+    function(a, b)
+}
+
+fn smart_pointers() {
+    let b_int1 = Box::new(10);
+    println!("{}", b_int1);
+
+    struct TreeNode<T> {
+        pub left: Option<Box<TreeNode<T>>>,
+        pub right: Option<Box<TreeNode<T>>>,
+        pub key: T,
+    }
+
+    impl<T> TreeNode<T> {
+        pub fn new(key: T) -> Self {
+            TreeNode {
+                left: None,
+                right: None,
+                key: key,
+            }
+        }
+        pub fn left(mut self, node: TreeNode<T>) -> Self {
+            self.left = Some(Box::new(node));
+            self
+        }
+        pub fn right(mut self, node: TreeNode<T>) -> Self {
+            self.right = Some(Box::new(node));
+            self
+        }
+    }
+
+    let root = TreeNode::new(1)
+        .left(TreeNode::new(2))
+        .right(TreeNode::new(3).right(TreeNode::new(99)));
+
+    // root.left.as_ref().unwrap().left(TreeNode::new(1000));
+    // --> cannot move out of a shared reference
+    // move occurs because value has type `TreeNode<i32>`, which does not implement the `Copy` trait
+
+    println!("{}", root.key);
+    println!("{}", root.left.as_ref().unwrap().key);
+    println!("{}", root.right.as_ref().unwrap().key);
+    println!(
+        "{}",
+        root.right.as_ref().unwrap().right.as_ref().unwrap().key
+    );
+
+    // println!("{}", root.left.unwrap().right.unwrap().key);
+    // -> thread 'main' panicked at 'called `Option::unwrap()` on a `None` value', src\main.rs:444:45
+}
+
+fn concurrency() {
+    let thread1 = thread::spawn(|| {
+        for i in 1..25 {
+            println!("Spawned thread '{}'", i);
+            thread::sleep(Duration::from_millis(1));
+        }
+    });
+
+    for i in 1..20 {
+        println!("Main thread: {}", i);
+        thread::sleep(Duration::from_millis(1));
+    }
+
+    thread1.join().unwrap();
+}
+
+fn concurrency_bank() {
+    pub struct Bank {
+        balance: f32,
+    }
+    // fn withdraw(bank: &mut Bank, amount: f32) {
+    //     bank.balance -= amount;
+    // }
+    // let mut bank = Bank { balance: 100.0 };
+    // withdraw(&mut bank, 5.0);
+    // println!("Balance: {}", bank.balance);
+
+    // fn customer(bank: &mut Bank) {
+    //     withdraw(bank, 5.0);
+    // }
+
+    // // Cannot have closure outlive current function. It borrows from it. -> smart pointer
+    // thread::spawn(|| customer(&mut bank)).join().unwrap();
+    // // -> may outlive borrowed value `bank`
+
+    fn withdraw(bank: &Arc<Mutex<Bank>>, amount: f32) {
+        let mut bank_ref = bank.lock().unwrap();
+
+        if bank_ref.balance < amount {
+            println!("No more money! {}", bank_ref.balance);
+        } else {
+            bank_ref.balance -= amount;
+            println!("Remaining balance: {}", bank_ref.balance);
+        }
+    }
+
+    fn customer(bank: Arc<Mutex<Bank>>) {
+        withdraw(&bank, 6.0);
+    }
+
+    let bank: Arc<Mutex<Bank>> = Arc::new(Mutex::new(Bank { balance: 61.0 }));
+    (0..15)
+        .map(|_| {
+            let bank_ref: Arc<Mutex<Bank>> = bank.clone();
+            thread::spawn(|| customer(bank_ref))
+        })
+        .for_each(|handle: thread::JoinHandle<()>| {
+            handle.join().unwrap();
+        });
+    println!("Total {}", bank.lock().unwrap().balance);
+}
+
 fn main() {
     // println!("{}, {}", MILLION, PI)
     // greet_user()
@@ -271,4 +545,14 @@ fn main() {
     // println!("{}", list_argument_by_ref(&vec![1, 2, 3, 100]));
     // println!("{}", generic_sum(10, 20));
     // println!("{}", generic_sum(9.9, 13.15));
+    // hashmap()
+    // structs()
+    // generic_struct()
+    // traits()
+    // restaurant::order_food()
+    // files()
+    // closure()
+    // smart_pointers()
+    // concurrency()
+    concurrency_bank()
 }
