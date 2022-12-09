8d3893f6cf57af11ec1909f053db6ade9203dbc4
Armin <halilovic.armin@hotmail.com>
2022-12-09 23:02:52 +0100

    AOC 2022 day 3 part 2 & unit tets

--- advent_of_code_2022/src/day_3.rs
+++ advent_of_code_2022/src/day_3.rs
@@ -1,6 +1,7 @@
 use std::collections::HashSet;
 use std::fs;
 use std::hash::Hash;
+use std::str::Lines;
 
 fn get_duplicates<T>(left: T, right: T) -> Vec<T::Item>
 where
@@ -25,11 +26,11 @@ where
 
 /// Lowercase item types a through z have priorities 1 through 26.
 /// Uppercase item types A through Z have priorities 27 through 52.
-fn get_priority(c: &char) -> u8 {
-    if c.is_lowercase() {
-        (*c as u8) - 'a' as u8 + 1
+fn get_priority(item: &char) -> u8 {
+    if item.is_lowercase() {
+        (*item as u8) - 'a' as u8 + 1
     } else {
-        (*c as u8) - 'A' as u8 + 1 + 26
+        (*item as u8) - 'A' as u8 + 1 + 26
     }
 }
 
@@ -37,24 +38,86 @@ fn get_priority(c: &char) -> u8 {
 /// (your puzzle input), but they need your help finding the errors. Every item
 /// type is identified by a single lowercase or uppercase letter (that is,
 /// a and A refer to different types of items).
-/// Find the item type that appears in both compartments of each rucksack.
+/// Part 1: Find the item type that appears in both compartments of each rucksack.
 /// What is the sum of the priorities of those item types?
+fn part_1_sum_of_priorities_in_rucksacks(lines: Lines) -> u32 {
+    let mut total: u32 = 0;
+    lines.for_each(|rucksack: &str| {
+        if rucksack.len() > 0 {
+            let (left, right): (&str, &str) = rucksack.split_at(rucksack.len() / 2);
+            let items: Vec<char> = get_duplicates(left.chars(), right.chars());
+            let priority: u8 = items.iter().fold(0, |acc, item| acc + get_priority(item));
+            println!(
+                "{:?}, {:?}, {:?}, {:?}, {:?}",
+                rucksack, left, right, items, priority
+            );
+            total += priority as u32;
+        }
+    });
+    println!("Sum of priorities in rucksacks: {}\n\n", total);
+    total
+}
+
+/// Part 2: Find the item type that corresponds to the badges of each three-Elf group.
+/// What is the sum of the priorities of those item types?
+fn part_2_sum_of_priorities_in_groups(lines: &mut Lines) -> u32 {
+    let mut total: u32 = 0;
+    while let (Some(l1), Some(l2), Some(l3)) = (lines.next(), lines.next(), lines.next()) {
+        let duplicates_l1_l2: String = get_duplicates(l1.chars(), l2.chars())
+            .into_iter()
+            .collect::<String>();
+        let badges: Vec<char> = get_duplicates(duplicates_l1_l2.chars(), l3.chars());
+        println!("{:?}, {:?}, {:?}, {:?}", l1, l2, l3, badges);
+        total += badges.iter().fold(0, |acc, c| acc + get_priority(c)) as u32;
+    }
+    println!("Sum of priorities in groups: {}", total);
+    total
+}
+
 fn main() {
     if let Ok(contents) = fs::read_to_string("inputs/day_3") {
-        let mut total: u32 = 0;
-        contents.lines().for_each(|rucksack| {
-            let nr_of_items = rucksack.len();
-            if nr_of_items > 0 {
-                let (left, right) = rucksack.split_at(nr_of_items / 2);
-                let dups = get_duplicates(left.chars(), right.chars());
-                let priority = dups.iter().fold(0, |acc, c| acc + get_priority(c));
-                println!(
-                    "{:?}, {:?}, {:?}, {:?}, {:?}",
-                    rucksack, left, right, dups, priority
-                );
-                total += priority as u32;
-            }
-        });
-        println!("{}", total);
+        part_1_sum_of_priorities_in_rucksacks(contents.lines());
+        part_2_sum_of_priorities_in_groups(&mut contents.lines());
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_1_single_line() {
+        assert_eq!(
+            part_1_sum_of_priorities_in_rucksacks("vJrwpWtwJgWrhcsFMMfFFhFp".lines()),
+            16
+        );
+        assert_eq!(
+            part_1_sum_of_priorities_in_rucksacks("PmmdzqPrVvPwwTWBwg".lines()),
+            42
+        );
+    }
+
+    #[test]
+    fn test_part_1_multi_line() {
+        let input = "vJrwpWtwJgWrhcsFMMfFFhFp
+jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+PmmdzqPrVvPwwTWBwg
+wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+ttgJtRGJQctTZtZT
+CrZsJsPPZsGzwwsLwLmpwMDw";
+
+        assert_eq!(part_1_sum_of_priorities_in_rucksacks(input.lines()), 157)
+    }
+
+    #[test]
+    fn test_part_2() {
+        let input = "vJrwpWtwJgWrhcsFMMfFFhFp
+jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+PmmdzqPrVvPwwTWBwg
+wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+ttgJtRGJQctTZtZT
+CrZsJsPPZsGzwwsLwLmpwMDw";
+
+        assert_eq!(part_2_sum_of_priorities_in_groups(&mut input.lines()), 70)
     }
 }
